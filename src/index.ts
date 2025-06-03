7c066d1936deab7d3e9ff7883a7ee14ee7d167b5
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-06-03 14:05:50 +0200

    Add initial implementation of Hono app with routes for object retrieval and counting

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { Hono } from "hono";
+
+interface Bindings {
+  KV: KVNamespace;
+  REGISTRY: Fetcher;
+}
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+const updateDB = async (kv: KVNamespace, objectName: string) => {
+  const counterKey = objectName.replace(".json", "");
+  const newCount = (Number(await kv.get(counterKey)) ?? 0) + 1;
+  await kv.put(counterKey, newCount.toString());
+};
+
+app.get("/r/:objectName", async (c) => {
+  const objectName = c.req.param("objectName");
+
+  if (!objectName) {
+    return c.json({ error: "Missing object name" }, 400);
+  }
+
+  try {
+    const registryUrl = `${new URL(c.req.url).origin}/static/${objectName}`;
+    const objectResponse = await c.env.REGISTRY.fetch(registryUrl);
+
+    if (!objectResponse.ok) {
+      return c.json({ error: "Object not found" }, 404);
+    }
+
+    c.executionCtx.waitUntil(updateDB(c.env.KV, objectName));
+
+    return objectResponse;
+  } catch (error) {
+    console.error("Error retrieving object:", error);
+    return c.json({ error: "Error processing request" }, 500);
+  }
+});
+
+app.get("/s/:objectName", async (c) => {
+  const objectName = c.req.param("objectName");
+
+  if (!objectName) {
+    return c.json({ error: "Missing object name" }, 400);
+  }
+  const counterKey = objectName.replace(".json", "");
+
+  const counter = Number(await c.env.KV.get(counterKey));
+
+  if (counter === 0) {
+    return c.json({ error: "Object not found" }, 404);
+  }
+
+  return c.json(
+    {
+      key: counterKey,
+      file: counterKey + ".json",
+      count: counter,
+    },
+    200,
+  );
+});
+
+export default app;
